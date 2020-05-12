0bf68a97ee878530d9b2b925476ee31b8d5c5ae0
Matt <nlmatt07@hotmail.com>
2020-05-12 13:09:12 -0300

    DTO

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -26,7 +26,10 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
