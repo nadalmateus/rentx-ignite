8d440c2ca969803782a1ed61ab445770e6086ac0
Matt <nlmatt07@hotmail.com>
2020-05-15 11:54:12 -0300

    Models + DB

--- src/database/migrations/1589310413495-CreateAppointments.ts
+++ src/database/migrations/1589310413495-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1589310413495
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1589482369249-CreateUsers.ts
+++ src/database/migrations/1589482369249-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1589482369249 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
