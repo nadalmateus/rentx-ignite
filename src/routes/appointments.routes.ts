01b9a596dd854caf676f3e6b054486f522b5cbff
Mateus Nadal <nlmatt07@hotmail.com>
2020-06-19 17:15:49 -0300

    Docker Config

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,29 +1,36 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
-
+import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../service/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = parseISO(date);
 
-  const findAppointmentsInSameDate = appointmentsRepository.findByDate(
-    parsedDate
-  );
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository
+    );
 
-  if (findAppointmentsInSameDate) {
-    return response
-      .status(400)
-      .json({ message: 'This Appointmet Is Alread Booked' });
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create(provider, parsedDate);
-
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
